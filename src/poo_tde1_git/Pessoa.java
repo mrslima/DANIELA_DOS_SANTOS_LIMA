7190073df4758a6cf1bd6e4e35c06279670812ec
mrslima <daniela.sl2f4@gmail.com>
2023-03-25 18:56:44 -0300

    Criar class Pessoa.

---
+++ src/poo_tde1_git/Pessoa.java
@@ -0,0 +1,11 @@
+package poo_tde1_git;
+
+public class Pessoa {
+	private String nome;
+	private int idade;
+	
+	public Pessoa(String nome, int idade) {
+		this.nome = nome;
+		this.idade = idade;
+	}
+}
